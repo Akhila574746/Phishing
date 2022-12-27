f4585747ae7f0b7851eafe657ff292f871adc153
Akhila574746 <87166392+Akhila574746@users.noreply.github.com>
2022-12-27 20:12:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,8 @@
 import streamlit as st
-
 import joblib
-
-model_nb = joblib.load('Fake_job_postings')
-vect = joblib.load('vect.pkl')
-
-def main():
-  st.title('Fake Job Detection') #creates a title in web app
-  ip = st.text_input('Enter Job Description:') #creates a text box in web app
-  if st.button('Predict'):
-    data=[ip]
-    cv=vect.transform(data).toarray()
-    prediction=model_nb.predict(cv)
-    result=prediction[0]
-    if result==0:
-      st.error("Fraudulant Job")
-    else:
-      st.success("Real Job")
-   
-main()  
+model_nb = joblib.load('Project')
+st.title('Phishing URL') #creates a title in web app
+ip = st.text_input('Enter URL:') #creates a text box in web app
+op = model_nb.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])
